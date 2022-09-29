ce16a98d09d791e39b479eae0091f20b4ecf8d80
JoshuaBog09 <95229035+JoshuaBog09@users.noreply.github.com>
2022-09-29 17:49:31 +0200

    Single turbine class added for determining the wake effects

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello world")
+import utils.constants as constants
 
+
+class Turbine:
+    """
+    """
+    def __init__(self, location, diameter, localwindspeed, power):
+        self.location = location
+        self.diameter = diameter
+        self.localwindspeed = localwindspeed
+        self.power = power
+        self.rotorspeed = (2/3)*self.localwindspeed
+        self.thrust = self.power / self.rotorspeed
+        self.ct = self.thrust / (0.5*constants.rho*(localwindspeed**2)*2*(self.diameter/2)**2)
+
+class Windfarm:
+    """
+    """
+    pass
+
+
+
+def main():
+    T1 = Turbine(0,200,5.14, constants.power)
+    print(T1.thrust)
 
 if __name__ == "__main__":
     main()
---
+++ utils/constants.py
@@ -0,0 +1,3 @@
+rho = 1.225  # kg/m^3
+power = 14*10**6  # W
+alpha = 0.05  # offshore
