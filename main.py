f2aac2df3b84889f0a7650d3d646f599b8c2a338
JoshuaBog09 <95229035+JoshuaBog09@users.noreply.github.com>
2022-10-02 16:28:24 +0200

    working version for wake effect downstream for x amount of turbines placed in a row/series (update deleted historic stuff)

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-# external libaries
+# external libraries
 import numpy as np
 import matplotlib.pyplot as plt
 
-# default libaries
+# default libraries
 import math
 
-#custom libaries
+# custom libraries
 import utils.constants as constants
 
 
@@ -27,16 +27,6 @@ class Turbine:
         self.thrust = self.power / self.rotorspeed
         # Thrust coefficient
         self.ct = self.thrust / (0.5*constants.rho*(localwindspeed**2)*math.pi*((diameter/2)**2))
-
-
-    # def evaluatespecific(self, idx):
-    #     return constants.windspeed * (1 - ((1 - math.sqrt(1 - self.ct)) / (1 + 2 * idx * constants.alpha) ** 2))
-    #
-    # def evaluateupto(self, nextturbine):
-    #     # nexttrubine is the distance form one turbine to the next one
-    #     # distance range
-    #     self.s = np.linspace((self.diameter * 3 + 1), nextturbine, 10000, endpoint=True) / self.diameter
-    #     self.unext = constants.windspeed * (1 - ((1 - math.sqrt(1 - self.ct)) / (1 + 2 * self.s * constants.alpha) ** 2))
 
     def supplyrange(self, first, last):
         self.s_range = np.linspace(first-self.location+(self.diameter * 3 + 1), last-self.location, 10000, endpoint=True) / self.diameter
@@ -74,8 +64,6 @@ def windfarm():
         else:
             spacing.append(2500)
 
-    print(spacing)
-
     # add the first turbine since it will always be at the start of the turbine chain (special 1 time operations)
     turbine_objs.append(Turbine(0, constants.diameter, constants.windspeed, constants.power))
     turbine_objs[0].printusefull()
@@ -101,51 +89,6 @@ def windfarm():
 
         plt.plot(x, y)
         plt.show()
-
-
-# def main():
-#
-#     T1 = Turbine(0,200, constants.windspeed, constants.power)
-#     print(T1.localwindspeed, T1.power, T1.thrust, T1.ct)
-#
-#     y = np.insert(T1.supplyrange(0, 2000), 0, constants.windspeed, axis=0)
-#     x = np.insert(T1.s_range*T1.diameter, 0, 0, axis=0)
-#
-#     plt.plot(x, y)
-#     plt.show()
-#
-#     T2 = Turbine(2000,200,T1.returnlast(), constants.power)
-#     print(T2.localwindspeed, T2.power, T2.thrust, T2.ct)
-#
-#     T2.setuto(constants.windspeed * (1 - np.sqrt(
-#         (1 - T1.supplyrange(2000, 4000) / constants.windspeed) ** 2 + (1 - T2.supplyrange(2000,4000) / constants.windspeed) ** 2)))
-#
-#     x = np.append(x, T2.location+(T2.s_range*T2.diameter))
-#     y = np.append(y, T2.u_range)
-#
-#     # write some function to do this combined wake madness (with as input a list of the participating objects)
-#
-#     plt.plot(x, y)
-#     plt.show()
-#
-#     T3 = Turbine(4000, 200, T2.returnlast(), constants.power)
-#     print(T3.localwindspeed, T3.power, T3.thrust, T3.ct)
-#     T3.setuto(constants.windspeed * (1 - np.sqrt(
-#         (1 - T1.supplyrange(4000, 6000) / constants.windspeed) ** 2 +
-#         (1 - T2.supplyrange(4000, 6000) / constants.windspeed) ** 2 +
-#         (1 - T3.supplyrange(4000, 6000) / constants.windspeed) ** 2)))
-#
-#     y = np.append(y, T3.u_range)
-#     x = np.append(x, T3.location + (T3.s_range * T3.diameter))
-#
-#     plt.plot(x, y)
-#     plt.show()
-#
-#     T4 = Turbine(6000, 200, T3.returnlast(), constants.power)
-#     print(T4.localwindspeed, T4.power, T4.thrust, T4.ct)
-#
-#     # find the turbines within proximity of first turbine (upto s=13)
-#     # store them in a list
 
 
 if __name__ == "__main__":
