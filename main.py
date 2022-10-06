b29b0d533bdca91cb4f1db2bb4fac23a4239d05d
JoshuaBog09 <95229035+JoshuaBog09@users.noreply.github.com>
2022-10-06 13:18:49 +0200

    documentation + raise error message for incorrect turbine spacing

--- main.py
+++ main.py
@@ -11,9 +11,18 @@ import utils.constants as constants
 
 class Turbine:
     """
+    Wind turbine class to store and calculate relevant values of the wind turbine.
+    velocity values in the wake are supplied in ranges from the turbine to the next to evaluate the wake and the mixed velocities.
     """
 
     def __init__(self, location: float, diameter: float, localwindspeed: float, power: float):
+        """
+        :param location: The location of the turbine in meters [m]
+        :param diameter: The diameter of the turbine used [m]
+        :param localwindspeed: The local windspeed [m/s]
+        :param power: The power[W] of the turbine based on the local windspeed.
+        Note this is not the rated power! however it is derived from it
+        """
 
         index = 0
         index += 1
@@ -29,51 +38,65 @@ class Turbine:
         self.ct = self.thrust / (0.5*constants.rho*(localwindspeed**2)*math.pi*((diameter/2)**2))
 
     def supplyrange(self, first, last):
+        """
+        :param first: The location of the current turbine that is to be analysed
+        :param last: The location of the next turbine
+        :return: The velocity list for the specified range for the requested instance
+        """
         self.s_range = np.linspace(first-self.location+(self.diameter * 3 + 1), last-self.location, 10000, endpoint=True) / self.diameter
         self.u_range = constants.windspeed * (1 - ((1 - math.sqrt(1 - self.ct)) / (1 + 2 * self.s_range * constants.alpha) ** 2))
         return self.u_range
 
     def setuto(self, velocities):
+        """
+        :param velocities: Set the velocity list equal to the mixed velocity called behind the turbine due to wake effects
+        :return: None
+        """
         self.u_range = velocities
 
     def returnlast(self):
+        """
+        :return: The last velocity element in the velocity list
+        """
         return self.u_range[-1]
 
     def printusefull(self):
+        """
+        Prints the usefull(windspeed[m/s^2], power[W], thrust[N] and thrust coeffiecient[-])
+        characteristics of the Turbine(...) instance.
+        """
         print(f"{self.localwindspeed=}", f"{self.power=}", f"{self.thrust=}", f"{self.ct=}")
 
     def __str__(self):
         return f"WindTurbine instance created with Turbine(location, diameter, localwindspeed, power)"
 
 
-class Windfarm:
-    """
-    """
-    pass
-
-
-def windfarm():
+def windfarm(request: list):
 
     turbine_objs = []
+    total_power = 0
 
-    request = [2000, 4000, 6000, 10000, 20000]  # requested turbine locations # 1, 1-2, 1-2-3
     spacing = []
     for i in range(len(request)):
         if i < len(request)-1:
-            spacing.append(request[i+1]-request[i])
+            space = request[i+1]-request[i]
+            if space <= 3*constants.diameter:
+                raise Exception(f"A turbine was placed to close to its neighbour."
+                                f"Distance of {space}m between turbine {i+1} and turbine {i+2} was identified"
+                                f"which is below the required min. 3*diameter")
+            spacing.append(space)
         else:
-            spacing.append(2500)
+            spacing.append(10*constants.diameter)
 
     # add the first turbine since it will always be at the start of the turbine chain (special 1 time operations)
     turbine_objs.append(Turbine(0, constants.diameter, constants.windspeed, constants.power))
     turbine_objs[0].printusefull()
 
-    y = np.insert(turbine_objs[0].supplyrange(0, 2000), 0, constants.windspeed, axis=0)
+    # add data points to plotting list
+    y = np.insert(turbine_objs[0].supplyrange(0, spacing[0]), 0, constants.windspeed, axis=0)
     x = np.insert(turbine_objs[0].s_range * turbine_objs[0].diameter, 0, 0, axis=0)
 
-    plt.plot(x, y)
-    plt.show()
-
+    # Evaluation of the velocities in the wake following jensen's model of mixed velocity.
     for location, distance in zip(request, spacing):
         turbine_objs.append(Turbine(location, constants.diameter, turbine_objs[-1].returnlast(), constants.power))
         turbine_objs[-1].printusefull()
@@ -84,12 +107,23 @@ def windfarm():
 
         turbine_objs[-1].setuto(constants.windspeed * (1 - np.sqrt(intermediate)))
 
+        # add data points to plotting list
         x = np.append(x, turbine_objs[-1].location + (turbine_objs[-1].s_range * turbine_objs[-1].diameter))
         y = np.append(y, turbine_objs[-1].u_range)
 
-        plt.plot(x, y)
-        plt.show()
+    # Plot
+    plt.plot(x, y)
+    plt.show()
+
+    for turbine in turbine_objs:
+        total_power += turbine.power
+
+    farm_efficiency = total_power / (len(turbine_objs)*constants.power)
+
+    print(total_power, farm_efficiency)
 
 
 if __name__ == "__main__":
-    windfarm()
+    request = [2000, 4000, 6000, 10000, 20000]  # requested turbine locations # 1, 1-2, 1-2-3
+    # request = [10000, 20000, 30000, 40000, 50000]
+    windfarm(request)
