acfb4741c12370d3ce0e061b63b535384b77ddce
JostiMeneer <wesselholtslag@hotmail.com>
2022-10-10 16:16:10 +0200

    Windspeed

--- Windspeed.py
+++ Windspeed.py
@@ -7,16 +7,17 @@ Z0 = 0.0002
 alpha = 0.11
 H_ref = 10
 V_Href = 0 #nader bepaald
-def loglaw(h):
-    V = V_Href *(h/Z0)/(H_ref/Z0)
-def powerlaw(alpha,h):
-    Href = 60
-    V = loglaw(Z0,60,h,V_Href) * ((h/H_ref))**2
+def V(alpha,h):
+    def loglaw(h):
+        V = V_Href *(h/Z0)/(H_ref/Z0)
+    def powerlaw(alpha,h):
+        Href = 60
+        V = loglaw(Z0,60,h,V_Href) * ((h/H_ref))**2
 
-H_ref_powerlaw = 61
-for i in range(0,H_ref_powerlaw):
-    loglaw(i)
+    H_ref_powerlaw = 61
+    for i in range(0,H_ref_powerlaw):
+        loglaw(i)
 
-Eind_hoogte= 0
-for i in range(60,Eind_hoogte):
-    powerlaw(alpha,i)
+    Eind_hoogte= 0
+    for i in range(60,Eind_hoogte):
+        powerlaw(alpha,i)
